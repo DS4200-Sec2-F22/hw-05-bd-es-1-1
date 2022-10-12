ff55383ee195e5699b8031913f1906f2e7bd57a6
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-10-12 17:07:09 -0400

    work

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 500;
+const FRAME_WIDTH = 550;
 
 const MARGINS = {left: 25, right: 25, top: 25, bottom: 25};
 
-const SCALE = d3.scaleLinear()
-  .domain(0, 9)
-  .range(0, 450)
-
-
-
+const SCALE = 50; 
 
 const FRAME1 = d3.select("#column1")
 .append("svg")
@@ -19,34 +14,54 @@ const FRAME1 = d3.select("#column1")
 
 
 d3.csv("data/scatter-data.csv").then( (data) => {
+  console.log(data)
+  
   FRAME1.selectAll("circle")
   .data(data)
   .enter()
   .append("circle")
-  .attr("cx", (d) => {return SCALE(d.x);})
-  .attr("cy", (d) => { return FRAME_HEIGHT - SCALE(d.y) })
+  .attr("cx", (d) => {return d.x * SCALE;})
+  .attr("cy", (d) => { return FRAME_HEIGHT - d.y * SCALE })
   .attr("r", 10)
   .attr("class", "point")
-
+  
 })
 
-VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom
-VIS_HEIGHT = FRAME_WIDTH - MARGINS.left - MARGINS.right
+const width = 500, height = 500;
+const svg = d3.select("#column1")
+.append("svg")
+.attr("width", width)
+.attr("height", height)
+.attr("class", "frame");
 
 
+const xscale = d3.scaleLinear()
+.domain([0, 100])
+.range([0, width - 50]);
 
+const x_axis = d3.axisBottom(xscale);
 
 FRAME1.append("g")
-.attr("transform", "translate(" + MARGINS.left + ","
-+ (VIS_HEIGHT + MARGINS.top) + ")")
-.call(d3.axisBottom(X_SCALE).ticks(9))
-.attr("font-size", "20px")
+.attr("transform", "translate(0,450)")
+.call(x_axis)
 
-FRAME1.append("g")
-  .attr("transform", "translate(" + (VIS_WIDTH + MARGINS.left) + ","
-    + MARGINS.top + ")")
-  .call(d3.axisLeft(SCALE).ticks(9))
-  .attr("font-size", "20px")
+
+// VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom
+// VIS_HEIGHT = FRAME_WIDTH - MARGINS.left - MARGINS.right
+
+
+
+// FRAME1.append("g")
+// .attr("transform", "translate(" + MARGINS.left + ","
+// + (VIS_HEIGHT + MARGINS.top) + ")")
+// .call(d3.axisBottom(X_SCALE).ticks(9))
+// .attr("font-size", "20px")
+
+// FRAME1.append("g")
+//   .attr("transform", "translate(" + (VIS_WIDTH + MARGINS.left) + ","
+//     + MARGINS.top + ")")
+//   .call(d3.axisLeft(Y_SCALE).ticks(9))
+//   .attr("font-size", "20px")
 
 
 
